4c751e71b9e02d990c8a6cbd828097a1e907b71f
Shafik Amin <shafik23@gmail.com>
2023-11-08 12:04:55 -0700

    Added more tests

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -61,7 +61,36 @@ func TestLexer(t *testing.T) {
 			input:    "10+20-30+40-50",
 			expected: []Token{INT, PLUS, INT, MINUS, INT, PLUS, INT, MINUS, INT},
 		},
-		// Add more complex test cases here as you expand your lexer's capabilities.
+		{
+			name:     "Identifiers",
+			input:    "x y z",
+			expected: []Token{IDENT, IDENT, IDENT},
+		},
+		{
+			name:     "Keywords",
+			input:    "fn let if else return",
+			expected: []Token{FUNCTION, LET, IF, ELSE, RETURN},
+		},
+		{
+			name:     "Operators",
+			input:    "= == ! != < >",
+			expected: []Token{ASSIGN, EQ, BANG, NOT_EQ, LT, GT},
+		},
+		{
+			name:     "Delimiters",
+			input:    ", ; ( ) { }",
+			expected: []Token{COMMA, SEMICOLON, LPAREN, RPAREN, LBRACE, RBRACE},
+		},
+		{
+			name:     "Mixed code",
+			input:    "let x = 5; if (x == 5) { x = x * 10; }",
+			expected: []Token{LET, IDENT, ASSIGN, INT, SEMICOLON, IF, LPAREN, IDENT, EQ, INT, RPAREN, LBRACE, IDENT, ASSIGN, IDENT, ASTERISK, INT, SEMICOLON, RBRACE},
+		},
+		{
+			name:     "Function declaration",
+			input:    "fn add(x, y) { return x + y; }",
+			expected: []Token{FUNCTION, IDENT, LPAREN, IDENT, COMMA, IDENT, RPAREN, LBRACE, RETURN, IDENT, PLUS, IDENT, SEMICOLON, RBRACE},
+		},
 	}
 
 	for _, tt := range tests {
