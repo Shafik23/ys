ac67a76fc96184ca0d030031f94d60ec019e0f73
Shafik Amin <shafik23@gmail.com>
2024-01-05 01:03:06 -0700

    Add Integer, Boolean, and Null object types

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,60 @@
+package evaluator
+
+import (
+	"testing"
+
+	"github.com/shafik23/ys/lexer"
+	"github.com/shafik23/ys/object"
+	"github.com/shafik23/ys/parser"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
+		{"5 + 5 + 5 + 5 - 10", 10},
+		{"2 * 2 * 2 * 2 * 2", 32},
+		{"-50 + 100 + -50", 0},
+		{"5 * 2 + 10", 20},
+		{"5 + 2 * 10", 25},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	// Create a new lexer and parser for each test case.
+	l := lexer.New(input)
+	p := parser.New(l)
+
+	ast := p.ParseProgram()
+
+	// Evaluate the program.
+	return Eval(ast)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	// Cast the object to an integer.
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	// Compare the value of the integer.
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+		return false
+	}
+
+	// Return true if the test passed.
+	return true
+}
--- object/object.go
+++ object/object.go
@@ -1,8 +1,44 @@
 package object
 
+import "fmt"
+
+const (
+	NULL_OBJ    = "NULL"
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+)
+
 type ObjectType string
 
 type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
+
+//////////////////////////////////////////////////
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+//////////////////////////////////////////////////
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+
+//////////////////////////////////////////////////
+
+type Null struct{}
+
+func (n *Null) Inspect() string { return "null" }
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
