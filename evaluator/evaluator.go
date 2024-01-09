0281d45b9fd914911676ad95742fa1314b718917
Shafik Amin <shafik23@gmail.com>
2024-01-08 23:31:16 -0700

    Add support for ! prefix expression in the evaluator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -21,6 +21,10 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		// Evaluate the right side of the expression.
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
@@ -34,6 +38,28 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	}
+
+	return NULL
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	}
+
+	return FALSE
 }
 
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -83,3 +83,24 @@ func testBooleanObject(t *testing.T, evaluated object.Object, expected bool) {
 		return
 	}
 }
+
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+
+	// Iterate over each test case.
+	for _, tt := range tests {
+		// Evaluate the input.
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
