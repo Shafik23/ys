c7f6f58bc9ec4808a02a176b55d6e61d9bb68730
Shafik Amin <shafik23@gmail.com>
2024-01-17 12:16:20 -0700

    Add HashKey implementation for Boolean, Integer, and String objects

--- object/object.go
+++ object/object.go
@@ -2,6 +2,7 @@ package object
 
 import (
 	"fmt"
+	"hash/fnv"
 
 	"github.com/shafik23/ys/ast"
 )
@@ -133,3 +134,31 @@ func (ao *Array) Inspect() string {
 }
 
 func (ao *Array) Type() ObjectType { return ARRAY_OBJ }
+
+//////////////////////////////////////////////////
+
+type HashKey struct {
+	Type  ObjectType
+	Value uint64
+}
+
+func (b *Boolean) HashKey() HashKey {
+	var value uint64
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+	return HashKey{Type: b.Type(), Value: value}
+}
+
+func (i *Integer) HashKey() HashKey {
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+
+func (s *String) HashKey() HashKey {
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+
+	return HashKey{Type: s.Type(), Value: h.Sum64()}
+}
